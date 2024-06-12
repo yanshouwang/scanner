9cef96951fc8f30b454aa92dda97f41a9f323adb
iAMD <yanshouwang@outlook.com>
2024-06-12 15:41:35 +0800

    Move background riv to AppView

--- lib/views/app_view.dart
+++ lib/views/app_view.dart
@@ -1,8 +1,10 @@
 import 'package:clover/clover.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:rive/rive.dart' as rive;
 import 'package:scanner/view_models.dart';
 
-class AppView extends StatelessWidget {
+class AppView extends StatefulWidget {
   final RouterConfig<Object> routerConfig;
 
   const AppView({
@@ -11,13 +13,101 @@ class AppView extends StatelessWidget {
   });
 
   @override
+  State<AppView> createState() => _AppViewState();
+}
+
+class _AppViewState extends State<AppView> {
+  rive.SMIBool? _themeInput;
+
+  bool get isDark {
+    final themeMode = ViewModel.of<AppViewModel>(context).themeMode;
+    if (themeMode == ThemeMode.system) {
+      return MediaQuery.platformBrightnessOf(context) == Brightness.dark;
+    }
+    return themeMode == ThemeMode.dark;
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final theme = ThemeData.light();
+    final darkTheme = ThemeData.dark();
     final viewModel = ViewModel.of<AppViewModel>(context);
     return MaterialApp.router(
-      routerConfig: routerConfig,
-      theme: ThemeData.light(),
-      darkTheme: ThemeData.dark(),
+      routerConfig: widget.routerConfig,
+      // Localization
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      onGenerateTitle: (context) => AppLocalizations.of(context)!.appName,
+      // Theme
       themeMode: viewModel.themeMode,
+      theme: theme.copyWith(
+        appBarTheme: theme.appBarTheme.copyWith(
+          backgroundColor: theme.colorScheme.surface.withOpacity(0.0),
+          foregroundColor: theme.colorScheme.onSurface,
+        ),
+        scaffoldBackgroundColor: theme.colorScheme.surface.withOpacity(0.0),
+      ),
+      darkTheme: darkTheme.copyWith(
+        appBarTheme: darkTheme.appBarTheme.copyWith(
+          backgroundColor: darkTheme.colorScheme.surface.withOpacity(0.0),
+          foregroundColor: darkTheme.colorScheme.onSurface,
+        ),
+        scaffoldBackgroundColor: theme.colorScheme.surface.withOpacity(0.0),
+      ),
+      // Background
+      builder: (context, child) {
+        return Stack(
+          children: [
+            DecoratedBox(
+              position: DecorationPosition.foreground,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Theme.of(context).colorScheme.surface.withOpacity(0.0),
+                    Theme.of(context).colorScheme.surface,
+                  ],
+                ),
+              ),
+              child: rive.RiveAnimation.asset(
+                'rivs/happycrm_greetings.riv',
+                fit: BoxFit.cover,
+                onInit: (artboard) {
+                  final themeController =
+                      rive.StateMachineController.fromArtboard(
+                    artboard,
+                    'State Machine 1',
+                  );
+                  if (themeController == null) {
+                    return;
+                  }
+                  artboard.addController(themeController);
+                  final themeInput =
+                      themeController.getBoolInput('Theme toggled');
+                  if (themeInput == null) {
+                    return;
+                  }
+                  _themeInput = themeInput..value = isDark;
+                },
+              ),
+            ),
+            child!,
+          ],
+        );
+      },
     );
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    final themeInput = _themeInput;
+    if (themeInput == null) {
+      return;
+    }
+    final isDark = this.isDark;
+    debugPrint('isDark: $isDark');
+    themeInput.value = isDark;
   }
 }
--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,60 +1,27 @@
 import 'package:clover/clover.dart';
 import 'package:flutter/material.dart';
-import 'package:rive/rive.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:scanner/view_models.dart';
 
-class HomeView extends StatefulWidget {
+class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
-  State<HomeView> createState() => _HomeViewState();
-}
-
-class _HomeViewState extends State<HomeView> {
-  SMIBool? _themeInput;
-
-  bool get isDark {
-    final themeMode = ViewModel.of<AppViewModel>(context).themeMode;
-    if (themeMode == ThemeMode.system) {
-      return MediaQuery.platformBrightnessOf(context) == Brightness.dark;
-    }
-    return themeMode == ThemeMode.dark;
-  }
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
+    final appLocalizations = AppLocalizations.of(context)!;
     final appViewModel = ViewModel.of<AppViewModel>(context);
     final themeMode = appViewModel.themeMode;
     return Scaffold(
-      body: Stack(
-        alignment: Alignment.topCenter,
+      appBar: AppBar(
+        title: Text(appLocalizations.appName),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          RiveAnimation.asset(
-            'rivs/happycrm_greetings.riv',
-            fit: BoxFit.cover,
-            onInit: (artboard) {
-              final themeController = StateMachineController.fromArtboard(
-                artboard,
-                'State Machine 1',
-              );
-              if (themeController == null) {
-                return;
-              }
-              artboard.addController(themeController);
-              final themeInput = themeController.getBoolInput('Theme toggled');
-              if (themeInput == null) {
-                return;
-              }
-              _themeInput = themeInput..value = isDark;
-            },
-          ),
+          const Spacer(),
           Container(
-            margin: const EdgeInsets.only(top: 40.0),
+            margin: const EdgeInsets.only(bottom: 40.0),
+            alignment: Alignment.bottomCenter,
             child: SegmentedButton(
               emptySelectionAllowed: false,
               multiSelectionEnabled: false,
@@ -74,17 +41,5 @@ class _HomeViewState extends State<HomeView> {
         ],
       ),
     );
-  }
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    final themeInput = _themeInput;
-    if (themeInput == null) {
-      return;
-    }
-    final isDark = this.isDark;
-    debugPrint('isDark: $isDark');
-    themeInput.value = isDark;
   }
 }
